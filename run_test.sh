b47d55b5ef8dcde3a1d2b061f3e458dc8f102e12
bharathappali <bharath.appali@gmail.com>
2022-09-05 01:08:53 +0530

    Running with 10000 jobs
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- run_test.sh
+++ run_test.sh
@@ -2,9 +2,9 @@
 
 if [[ $(which docker) && $(docker --version) ]]; then
     echo "Starting experiment with native threads."
-    docker run --rm -it bharathappali/jvtt:latest
+    docker run --rm -it -e NUM_NATIVE_THREAD=100 -e NUM_JOBS=10000 bharathappali/jvtt:latest
     echo "Starting experiment with virtual threads"
-    docker run --rm -it -e USE_VIRTUAL="true" bharathappali/jvtt:latest
+    docker run --rm -it -e USE_VIRTUAL="true" -e NUM_NATIVE_THREAD=100 -e NUM_JOBS=10000 bharathappali/jvtt:latest
 else
     echo "Docker not installed. Exiting"
     exit 1
